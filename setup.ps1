201b92fded35089b362c7656949ada454c23adff
ycookiey <70356861+ycookiey@users.noreply.github.com>
2026-01-09 14:38:02 +0900

    yaziのシンボリックリンク範囲を拡張

--- setup.ps1
+++ setup.ps1
@@ -4,4 +4,4 @@ ni -ItemType Directory "$HOME\.config" -Force | Out-Null
 ni -ItemType SymbolicLink -Path "$HOME\.config\wezterm" -Target "$PSScriptRoot\wezterm" -Force | Out-Null
 
 ni -ItemType Directory "$env:APPDATA\yazi\config" -Force | Out-Null
-ni -ItemType SymbolicLink -Path "$env:APPDATA\yazi\config\yazi.toml" -Target "$PSScriptRoot\yazi\yazi.toml" -Force | Out-Null
+ni -ItemType SymbolicLink -Path "$env:APPDATA\yazi\config" -Target "$PSScriptRoot\yazi" -Force | Out-Null
