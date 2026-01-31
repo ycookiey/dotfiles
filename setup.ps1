3929ec5be607ce8cced8dfdf81838bcc41a2909d
ycookiey <70356861+ycookiey@users.noreply.github.com>
2026-01-31 22:08:13 +0900

    add relevant symboliclink

--- setup.ps1
+++ setup.ps1
@@ -5,3 +5,12 @@ ni -ItemType SymbolicLink -Path "$HOME\.config\wezterm" -Target "$PSScriptRoot\w
 
 ni -ItemType Directory "$env:APPDATA\yazi\config" -Force | Out-Null
 ni -ItemType SymbolicLink -Path "$env:APPDATA\yazi\config" -Target "$PSScriptRoot\yazi" -Force | Out-Null
+
+ni -ItemType SymbolicLink -Path "$env:LOCALAPPDATA\nvim" -Target "$PSScriptRoot\config\nvim" -Force | Out-Null
+
+ni -ItemType Directory "$env:LOCALAPPDATA\lazygit" -Force | Out-Null
+ni -ItemType SymbolicLink -Path "$env:LOCALAPPDATA\lazygit\config.yml" -Target "$PSScriptRoot\lazygit\config.yml" -Force | Out-Null
+
+ni -ItemType Directory "$HOME\.claude" -Force | Out-Null
+ni -ItemType SymbolicLink -Path "$HOME\.claude\statusline.ps1" -Target "$PSScriptRoot\bin\claude-statusline.ps1" -Force | Out-Null
+ni -ItemType SymbolicLink -Path "$HOME\.claude\settings.json" -Target "$PSScriptRoot\claude\settings.json" -Force | Out-Null
