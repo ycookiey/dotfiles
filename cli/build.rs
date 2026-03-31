33e74410dad1f4d291eb8bb8d687441d33ccb9d0
ycookiey <70356861+ycookiey@users.noreply.github.com>
2026-03-31 11:14:50 +0900

    Fix dotb not finding projects by using absolute DOTFILES_DIR

--- cli/build.rs
+++ cli/build.rs
@@ -1,3 +1,5 @@
 fn main() {
-    println!("cargo:rustc-env=DOTFILES_DIR=..");
+    let manifest = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    let dotfiles = std::path::Path::new(&manifest).parent().unwrap();
+    println!("cargo:rustc-env=DOTFILES_DIR={}", dotfiles.display());
 }
