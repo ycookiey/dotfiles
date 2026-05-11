ef8e1ddc436f6223a7f3cfb713ae431d5e1a020c
ycookiey <70356861+ycookiey@users.noreply.github.com>
2026-05-11 10:45:49 +0900

    fix(nvim): migrate nvim-treesitter to main branch new API

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,17 +1,26 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  branch = "master",
-  event = { "BufReadPre", "BufNewFile" },
-  build = ":TSUpdate",
+  branch = "main",
+  lazy = false,
+  build = function()
+    require("nvim-treesitter").update()
+  end,
   config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "lua", "javascript", "typescript", "python", "json", "yaml", "html", "css",
-        "markdown", "markdown_inline",
+    local parsers = {
+      "lua", "javascript", "typescript", "python", "json", "yaml", "html", "css",
+      "markdown", "markdown_inline",
+    }
+    require("nvim-treesitter").install(parsers)
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = {
+        "lua", "javascript", "typescript", "tsx", "python", "json", "yaml",
+        "html", "css", "markdown",
       },
-      highlight = { enable = true },
-      indent = { enable = true },
-      matchup = { enable = true },
+      callback = function(args)
+        pcall(vim.treesitter.start)
+        vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
     })
   end,
 }
